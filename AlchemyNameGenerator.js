193d06245fa392aa21b9c1e82f10d678fe2df230
farben <>
2021-04-16 15:53:51 +0200

    Formatting

--- AlchemyNameGenerator.js
+++ AlchemyNameGenerator.js
@@ -1,38 +1,43 @@
 var synonyms = require("synonyms");
 var seedrandom = require("seedrandom");
+var synonyms = require("synonyms");
 
 module.exports = {
-    
-    getAlchemyFusionName :function (mainItem,otherItemsName, captchaCode, type) {
-        //safeguards in case the funcytion is used wrongly
-        if (!otherItemsName)
-            return mainItem
-        if (otherItemsName.length === 0)
-            return mainItem
-        
-        var rng = seedrandom(captchaCode);
+  /**
+   *
+   * @param {*} mainItemName The name of the item deciding the kind of the alchemised item
+   * @param {*} otherItemsName An array containing the name of the items fused with the main item
+   * @param {*} captchaCode captchaCode of the alchemised item
+   * @param {*} type
+   * @returns  {*} The name of the alchemised item
+   */
+  getAlchemyFusionName: function (
+    mainItemName,
+    otherItemsName,
+    captchaCode,
+    type
+  ) {
+    //safeguards in case the funcytion is used wrongly
+    if (!otherItemsName) return mainItemName;
+    if (otherItemsName.length === 0) return mainItemName;
 
-        otherItemsName.sort();
+    var rng = seedrandom(captchaCode);
 
-        let cutOtherItemsName = otherItemsName.map( word => getCutWord(word))
+    otherItemsName.sort();
 
-        console.log(cutOtherItemsName);
-        
-
-    }
-}
+    let parsedOtherItemNames = otherItemsName.map((word) => getCutWord(word));
+  },
+};
 
 function getCutWord(word) {
-    //separate camel case words ("ExampleWordJumble" -> "Example Word Jumble")
-    let cutWord = word.replace(/([A-Z])/g, ' $1')
-        .replace(/^./, function (str) { return str.toUpperCase(); })
-    
-    console.log(cutWord)
-    
-    let cutWordArray = cutWord.trim().split(cutWord, " ")
-    
-    console.log(cutWordArray);
+  //separate camel case words ("ExampleWordJumble" -> "Example Word Jumble")
+  let cutWord = word.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return str.toUpperCase();
+  });
 
-    return cutWordArray;
+  let cutWordArray = cutWord.trim().split(" ");
+
+  return cutWordArray;
 }
 
+function getRandomSynonym(word) {}
--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-
 var ang = require("./AlchemyNameGenerator.js");
-var synonyms = require("synonyms");
-
 
 console.log(ang.getAlchemyFusionName("Sword", ["dog"], "eLsmpk42", "e"));
-
-
-
-
-
